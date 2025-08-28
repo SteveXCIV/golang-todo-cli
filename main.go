753a1d944f2195ceb137429bef19bff2fd57d13a
Steve Troetti <SteveXCIV@gmail.com>
2025-08-28 09:53:49 -0400

    split tasks to separate package

--- main.go
+++ main.go
@@ -4,124 +4,24 @@ import (
 	"encoding/json"
 	"fmt"
 	"time"
+
+	"github.com/stevexciv/golang-todo-cli/tasks"
 )
-
-type Priority int
-
-const (
-	Low Priority = iota
-	Medium
-	High
-)
-
-var priorityToString = map[Priority]string{
-	Low:    "LOW",
-	Medium: "MEDIUM",
-	High:   "HIGH",
-}
-
-var stringToPriority = map[string]Priority{
-	"LOW":    Low,
-	"MEDIUM": Medium,
-	"HIGH":   High,
-}
-
-func (p *Priority) String() string {
-	return priorityToString[*p]
-}
-
-func (p Priority) MarshalJSON() ([]byte, error) {
-	if str, ok := priorityToString[p]; ok {
-		return json.Marshal(str)
-	}
-	return nil, fmt.Errorf("unknown priority: %d", p)
-}
-
-func (p *Priority) UnmarshalJSON(b []byte) error {
-	if val, ok := stringToPriority[string(b)]; ok {
-		*p = val
-		return nil
-	}
-	return fmt.Errorf("unknown priority: %s", string(b))
-}
-
-type Status int
-
-const (
-	Pending Status = iota
-	Completed
-)
-
-var statusToString = map[Status]string{
-	Pending:   "pending",
-	Completed: "completed",
-}
-
-var stringToStatus = map[string]Status{
-	"pending":   Pending,
-	"completed": Completed,
-}
-
-func (s *Status) String() string {
-	if str, ok := statusToString[*s]; ok {
-		return str
-	}
-	return "UNKNOWN"
-}
-
-func (s Status) MarshalJSON() ([]byte, error) {
-	if str, ok := statusToString[s]; ok {
-		return json.Marshal(str)
-	}
-	return nil, fmt.Errorf("unknown status: %d", s)
-}
-
-func (s *Status) UnmarshalJSON(b []byte) error {
-	if val, ok := stringToStatus[string(b)]; ok {
-		*s = val
-		return nil
-	}
-	return fmt.Errorf("unknown status: %s", string(b))
-}
-
-type DueDate time.Time
-
-func (d DueDate) MarshalJSON() ([]byte, error) {
-	return json.Marshal(time.Time(d).Format("2006-01-02"))
-}
-
-func (d *DueDate) UnmarshalJSON(b []byte) error {
-	if timestamp, err := time.Parse(`"2006-01-02"`, string(b)); err != nil {
-		return err
-	} else {
-		*d = DueDate(timestamp)
-		return nil
-	}
-}
-
-type Task struct {
-	Id       int      `json:"id"`
-	Title    string   `json:"title"`
-	Priority Priority `json:"priority"`
-	DueDate  DueDate  `json:"dueDate"`
-	Category string   `json:"category"`
-	Status   Status   `json:"status"`
-}
 
 func main() {
-	var testTask = Task{
+	var testTask = tasks.Task{
 		Id:       1,
 		Title:    "Schedule dentist appointment",
-		Priority: High,
-		DueDate:  DueDate(time.Now().AddDate(0, 0, 7)),
+		Priority: tasks.High,
+		DueDate:  tasks.DueDate(time.Now().AddDate(0, 0, 7)),
 		Category: "Health",
-		Status:   Pending,
+		Status:   tasks.Pending,
 	}
 
-	jsonStatus, _ := json.Marshal(Completed)
+	jsonStatus, _ := json.Marshal(tasks.Completed)
 	fmt.Println("Marshaled Status:", string(jsonStatus))
 
-	var parsedStatus Status
+	var parsedStatus tasks.Status
 	_ = json.Unmarshal(jsonStatus, &parsedStatus)
 	fmt.Println("Parsed Status:", parsedStatus)
 
---
+++ tasks/task.go
@@ -0,0 +1,113 @@
+package tasks
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Priority int
+
+const (
+	Low Priority = iota
+	Medium
+	High
+)
+
+var priorityToString = map[Priority]string{
+	Low:    "LOW",
+	Medium: "MEDIUM",
+	High:   "HIGH",
+}
+
+var stringToPriority = map[string]Priority{
+	"LOW":    Low,
+	"MEDIUM": Medium,
+	"HIGH":   High,
+}
+
+func (p *Priority) String() string {
+	return priorityToString[*p]
+}
+
+func (p Priority) MarshalJSON() ([]byte, error) {
+	if str, ok := priorityToString[p]; ok {
+		return json.Marshal(str)
+	}
+	return nil, fmt.Errorf("unknown priority: %d", p)
+}
+
+func (p *Priority) UnmarshalJSON(b []byte) error {
+	if val, ok := stringToPriority[string(b)]; ok {
+		*p = val
+		return nil
+	}
+	return fmt.Errorf("unknown priority: %s", string(b))
+}
+
+type Status int
+
+const (
+	Pending Status = iota
+	Completed
+)
+
+var statusToString = map[Status]string{
+	Pending:   "pending",
+	Completed: "completed",
+}
+
+var stringToStatus = map[string]Status{
+	"pending":   Pending,
+	"completed": Completed,
+}
+
+func (s *Status) String() string {
+	if str, ok := statusToString[*s]; ok {
+		return str
+	}
+	return "UNKNOWN"
+}
+
+func (s Status) MarshalJSON() ([]byte, error) {
+	if str, ok := statusToString[s]; ok {
+		return json.Marshal(str)
+	}
+	return nil, fmt.Errorf("unknown status: %d", s)
+}
+
+func (s *Status) UnmarshalJSON(b []byte) error {
+	var k string
+	if err := json.Unmarshal(b, &k); err != nil {
+		return fmt.Errorf("status should be a string, got %s", string(b))
+	}
+	if val, ok := stringToStatus[k]; ok {
+		*s = val
+		return nil
+	}
+	return fmt.Errorf("unknown status: %s", k)
+}
+
+type DueDate time.Time
+
+func (d DueDate) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.Time(d).Format("2006-01-02"))
+}
+
+func (d *DueDate) UnmarshalJSON(b []byte) error {
+	if timestamp, err := time.Parse(`"2006-01-02"`, string(b)); err != nil {
+		return err
+	} else {
+		*d = DueDate(timestamp)
+		return nil
+	}
+}
+
+type Task struct {
+	Id       int      `json:"id"`
+	Title    string   `json:"title"`
+	Priority Priority `json:"priority"`
+	DueDate  DueDate  `json:"dueDate"`
+	Category string   `json:"category"`
+	Status   Status   `json:"status"`
+}
