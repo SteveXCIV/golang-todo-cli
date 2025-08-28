86bcf02c482732ebdc8d5746f2c0b9a7b26d000e
Steve Troetti <SteveXCIV@gmail.com>
2025-08-27 22:18:11 -0400

    design initial task struct

--- todo.go
+++ todo.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Priority int
+
+const (
+	Low Priority = iota
+	Medium
+	High
+)
+
+type Status int
+
+const (
+	Pending Status = iota
+	Completed
+)
+
+type task struct {
+	Id       int       `json:"id"`
+	Title    string    `json:"title"`
+	Priority Priority  `json:"priority"`
+	DueDate  time.Time `json:"dueDate"`
+	Category string    `json:"category"`
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	var testTask = task{
+		Id:       1,
+		Title:    "Schedule dentist appointment",
+		Priority: High,
+		DueDate:  time.Now().AddDate(0, 0, 7),
+		Category: "Health",
+	}
+	jsonData, _ := json.Marshal(testTask)
+	fmt.Println(string(jsonData))
 }
