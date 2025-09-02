e01b49a8e15886b41013e0de7dd077e3c5f5f0b2
Steve Troetti <SteveXCIV@gmail.com>
2025-09-02 01:20:37 -0400

    implement search

--- cli/search.go
+++ cli/search.go
@@ -1,11 +1,26 @@
 package cli
 
-import "github.com/stevexciv/golang-todo-cli/tasks"
+import (
+	"strings"
+
+	"github.com/stevexciv/golang-todo-cli/tasks"
+)
 
 type SearchCommand struct {
 	Query string
 }
 
 func (s *SearchCommand) Execute(m tasks.Manager) (string, error) {
-	panic("not implemented")
+	tasks, err := m.SearchTasks(s.Query)
+	if err != nil {
+		return "", err
+	}
+	builder := strings.Builder{}
+	for i, task := range tasks {
+		builder.WriteString(task.Title)
+		if i < len(tasks)-1 {
+			builder.WriteString("\n")
+		}
+	}
+	return builder.String(), nil
 }
