81dbfa768b5fb280fc031bf22cb8b835ff0b973a
Steve Troetti <SteveXCIV@gmail.com>
2025-09-02 00:25:02 -0400

    implement list

--- cli/list.go
+++ cli/list.go
@@ -1,6 +1,11 @@
 package cli
 
-import "github.com/stevexciv/golang-todo-cli/tasks"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/stevexciv/golang-todo-cli/tasks"
+)
 
 type ListCommand struct {
 	StatusFilter   *tasks.Status
@@ -10,5 +15,13 @@ type ListCommand struct {
 }
 
 func (l *ListCommand) Execute(m tasks.Manager) (string, error) {
-	panic("not implemented!")
+	tasks, err := m.ListTasks(l.StatusFilter, l.PriorityFilter, l.CategoryFilter, l.OverdueFilter)
+	if err != nil {
+		return "", err
+	}
+	var sb strings.Builder
+	for _, t := range tasks {
+		sb.WriteString(fmt.Sprint(t) + "\n")
+	}
+	return sb.String(), nil
 }
